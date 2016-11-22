63ea230b9382b1f365aa8a4ca38d2689a2b0dfb8
cool-Blue <cool.blue@y7mail.com>
2016-11-23 02:16:54 +1100

    initial

---
+++ CH10.UserControls/ColorPicker.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CH10.UserControls
+{
+	/// <summary>
+	/// Interaction logic for ColorPicker.xaml
+	/// </summary>
+	public partial class ColorPicker : UserControl
+	{
+		public ColorPicker()
+		{
+			InitializeComponent();
+		}
+		
+		// dp for selected color
+		//   POCO backing field
+		public Color SelectedColor
+		{
+			get { return (Color) GetValue(SelectedColorProperty); }
+			set { SetValue(SelectedColorProperty, value); }
+		}
+
+		//   register it in WPF
+		public static readonly DependencyProperty 
+			SelectedColorProperty = DependencyProperty.Register("SelectedColor", typeof(Color),
+				typeof(ColorPicker), new UIPropertyMetadata(Colors.Black, OnSelectedColorChanged));
+
+		// routed event for color selection changed
+		//   register it in the WPF routed events system, 
+		//   ascociate it with ColorPicker and keep a reference
+		public static RoutedEvent SelectedColorChangedEvent =
+			EventManager.RegisterRoutedEvent("SelectedColorChanged", RoutingStrategy.Bubble,
+			typeof(RoutedPropertyChangedEventHandler<Color>), typeof(ColorPicker));
+
+		//   define the CLR event and defer to the WPF subscription management
+		//   todo understand this better
+		public event RoutedPropertyChangedEventHandler<Color> SelectedColorChanged
+		{
+			add { AddHandler(SelectedColorChangedEvent, value); }
+			remove { RemoveHandler(SelectedColorChangedEvent, value); }
+		}
+
+		static void OnSelectedColorChanged(DependencyObject obj,
+			DependencyPropertyChangedEventArgs e)
+		{
+			var cp = (ColorPicker) obj;
+			cp.RaiseEvent(new RoutedPropertyChangedEventArgs<Color>( (Color)e.OldValue, 
+				(Color)e.NewValue, SelectedColorChangedEvent));
+		}
+
+	}
+}
---
+++ CH10.UserControls/ColorToDoubleConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CH10.UserControls
+{
+	class ColorToDoubleConverter :IValueConverter
+	{
+		Color _lastColor;
+
+		public object Convert (object value, Type targetType, object parameter,
+			CultureInfo culture)
+		{
+			var color = (Color)value;
+			_lastColor = color;
+			switch ((string)parameter)
+			{
+				case "r": return color.R;
+				case "g": return color.G;
+				case "b": return color.B;
+				case "a": return color.A;
+			}
+			return Binding.DoNothing;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter,
+			CultureInfo culture)
+		{
+			Color color = _lastColor;
+			var intensity = (byte)(double)value;
+			switch ((string)parameter)
+			{
+				case "r":
+					color.R = intensity;
+					break;
+				case "g":
+					color.G = intensity;
+					break;
+				case "b":
+					color.B = intensity;
+					break;
+				case "a":
+					color.A = intensity;
+					break;
+			}
+			_lastColor = color;
+			return color;
+		}
+	}
+}
